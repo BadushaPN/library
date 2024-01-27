1e9c1bdde8fe4863b8ced7f2d49dafa0417a255d
BadushaPN <badushapn4@gmail.com>
2024-01-27 19:02:25 +0530

    updated

--- lib/view/menu_screen.dart
+++ lib/view/menu_screen.dart
@@ -29,7 +29,7 @@ class MenuScreen extends StatelessWidget {
       body: Column(
         mainAxisAlignment: MainAxisAlignment.center,
         children: [
-          const Text(
+          Text(
             "Badusha",
             overflow: TextOverflow.clip,
             style: TextStyle(fontWeight: FontWeight.bold, fontSize: 50),
